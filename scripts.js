1a8ec1f423be5725a2b71a4d25d5662b34c40a3f
Gabria Gordon <gabria464@gmail.com>
2025-11-23 22:23:00 -0500

    Final project completion - all features implemented

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+window.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector(".newsletter form");
+    const emailInput = document.getElementById("email");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const emailValue = emailInput.value.trim();
+
+         if (emailValue === "" || !isValidEmail(emailValue)) {
+            messageDiv.textContent = "Please enter a valid email address.";
+         } else {
+            messageDiv.textContent = `Thank you! Your email address ${emailValue} has been added to our mailing list!`;
+            emailInput.value = "";
+         }
+    });
+
+    function isValidEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    
+    }
+
+
+});
+
